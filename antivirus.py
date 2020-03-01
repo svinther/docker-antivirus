7b607c78f68f6bee13972c6a74d6e8e4f656aabf
Steffen Vinther Sørensen <svs@logiva.dk>
2020-03-01 12:17:03 +0100

    simplify and avoid using tmp file

--- antivirus.py
+++ antivirus.py
@@ -1,56 +1,33 @@
 #!/usr/bin/python3
 
 import cgi
-import os
 import subprocess
 import sys
-import tempfile
 
-import argparse
+CGI_FORM_FILETOCHECK = 'filetocheck'
 
-parser = argparse.ArgumentParser(description='Scan input for virus using clamav')
-parser.add_argument("fromfile", nargs='?', help='Scan a local file, if positional argument is not present, assume CGI')
-args = parser.parse_args()
+print("Content-Type: text/plain; charset=UTF-8")
+print()
 
-CGI_FORM_FILETOCHECK='filetocheck'
+form = cgi.FieldStorage()
+fileitem = form[CGI_FORM_FILETOCHECK] if CGI_FORM_FILETOCHECK in form else None
+if fileitem is not None and fileitem.file is not None:
+    realfilename = fileitem.filename
 
-iscgi = False
-if not args.fromfile:
-    print("Content-Type: text/plain; charset=UTF-8")
-    print()
+    p = subprocess.Popen('/usr/bin/clamdscan --verbose --stdout --multiscan --fdpass -'.split(),
+                         stderr=subprocess.STDOUT,
+                         stdout=subprocess.PIPE,
+                         stdin=subprocess.PIPE,
+                         bufsize=8192)
+    stdout, stderr = p.communicate(input=fileitem.file.read())
+    sys.stdout.flush()
+    print(stdout.decode("utf-8"))
+    rv = p.wait()
 
-    iscgi = True
-    form = cgi.FieldStorage()
-
-    fileitem = form[CGI_FORM_FILETOCHECK] if CGI_FORM_FILETOCHECK in form else None
-    if fileitem is not None and fileitem.file is not None:
-        tmpfile = tempfile.NamedTemporaryFile(delete=False)
-        tmpfile.write(fileitem.file.read())
-        tmpfile.close()
-        # os.fchmod(tmpfile, 644)
-        filetocheck = tmpfile.name
-        realfilename=fileitem.filename
-    else:
-        print("Invalid request - try /avform for testing antivirus")
-        raise Exception("CGI mode assumed, but no form field 'filetocheck' present")
-
+    print("------------------------------------")
+    print("clamscan exit code: %s" % rv)
+    print("real filename was: %s" % realfilename)
+    print("__AVSTATUS__= %s " % ("OK" if rv == 0 else "VIRUS" if rv == 1 else "FAIL"))
 else:
-    filetocheck = args.fromfile
-    realfilename = filetocheck
-
-p = subprocess.Popen('/usr/bin/clamdscan --verbose --stdout --multiscan --fdpass'.split() + [filetocheck],
-                     stderr=subprocess.STDOUT,
-                     stdout=subprocess.PIPE,
-                     bufsize=8192)
-
-sys.stdout.flush()
-print(p.stdout.read().decode("utf-8"))
-rv=p.wait()
-
-print("------------------------------------")
-print("clamscan exit code: %s" % rv)
-print("real filename was: %s" % realfilename)
-print("__AVSTATUS__= %s " % ("OK" if rv == 0 else "VIRUS" if rv == 1 else "FAIL"))
-
-if iscgi:
-    os.remove(filetocheck)
+    print("Invalid request - try /avform for testing antivirus")
+    raise Exception("CGI mode assumed, but no form field 'filetocheck' present")
