4b9630a98f87e25d5ea29b2eadbfa276b66a972f
Steffen Vinther Sørensen <svs@logiva.dk>
2020-03-01 12:32:07 +0100

    do chown at runtime

--- 40-start-freshclam.sh
+++ 40-start-freshclam.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#it is not working for all volume kinds, by doing this from Dockerfile only
+chown clamav:clamav /var/lib/clamav
+
 #Make sure the database is initialized
 echo "Running freshclam"
 freshclam && touch /var/lib/clamav/initialized.ok
